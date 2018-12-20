ccadcda4b606763ee547bbeae722cdf8c7f52926
PuZZleDucK <puzzleduck@gmail.com>
2018-12-21 10:51:40 +1100

    Feature: first name stored

---
+++ NameSorter/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Models
+{
+    public class Person
+    {
+        private string firstName;
+
+        public void setFirstName(string newName) {
+          firstName = newName;
+        }
+
+        public string getFullName() {
+          return firstName;
+        }
+    }
+}

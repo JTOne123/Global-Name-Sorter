be5e540d4f8dc6ac3d2af4f2bb182afcb703db17
PuZZleDucK <puzzleduck@gmail.com>
2018-12-21 13:46:11 +1100

    Feature: sorting single item

--- NameSorter/NameSorter.cs
+++ NameSorter/NameSorter.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using Models;
+using System.Collections.Generic;
 
 namespace NameSorter
 {
     public class NameSorter
     {
+      private List<Person> inputNames;
+      private List<Person> sortedNames;
+
+      public NameSorter() {
+        inputNames = new List<Person>();
+      }
+
+      public void addPerson(Person newPerson) {
+        inputNames.Add(newPerson);
+      }
+
+      public List<Person> sortPeople() {
+        sortedNames = inputNames;
+        return sortedNames;
+      }
     }
 }

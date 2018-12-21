805dec9875190a066d76685c48c65a25d728c2ba
PuZZleDucK <puzzleduck@gmail.com>
2018-12-21 11:04:27 +1100

    Test: multiple first names

--- NameSorter.Tests/UnitTests.cs
+++ NameSorter.Tests/UnitTests.cs
@@ -7,6 +7,14 @@ namespace NameSorter.Tests
     public class UnitTests
     {
         [Fact]
+        public void PersonFirstNamesFunctionsAsExpected()
+        {
+            Person person = new Person();
+            person.setFirstNames("First", "Second", "Third");
+            Assert.Equal("First Second Third", person.getFullName());
+        }
+
+        [Fact]
         public void PersonFirstNameFunctionsAsExpected()
         {
             Person person = new Person();
